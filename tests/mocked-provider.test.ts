205ebc606438566501cd33b9a29264ec8ab359bb
Jasper Verbeet <jasperverbeet@gmail.com>
2022-11-11 12:18:39 +0100

    test: adds test for event mocking events

--- tests/mocked-provider.test.ts
+++ tests/mocked-provider.test.ts
@@ -105,4 +105,53 @@ describe('MockedProvider', () => {
       await contract.balanceOf('0x3845874ec9df670d54eb3edebd33b18353905a44')
     ).toEqual(BigNumber.from('2'))
   })
+
+  it('should mock listener after registering events', async () => {
+    /** Mocked ERC721 Transfer event return values */
+    const contractAddress = '0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d'
+    const fromAddress = '0x31B0C4112A9AA5B79cA5883465bFC4Cd013c6282'
+    const toAddress = '0x31B0C4112A9AA5B79cA5883465bFC4Cd013c6282'
+    const tokenId = BigNumber.from('999999999')
+
+    /** Create a new mock provider */
+    const mockProvider = new MockedProvider({ chainId: 1, name: 'mainnet' })
+
+    /**
+     * Create a mocked listener, later we can test if it was called with the
+     * correct arguments
+     */
+    const mockedListener = jest.fn()
+
+    mockProvider.mockEvent({
+      abi: erc721Abi,
+      address: contractAddress,
+      /** We assume a standard ERC721 */
+      eventName: 'Transfer',
+      returnValue: [fromAddress, toAddress, tokenId],
+    })
+
+    /** Register the listener with Ethers */
+    const contract = new Contract(contractAddress, erc721Abi, mockProvider)
+    contract.on('Transfer', mockedListener)
+
+    /**
+     * Events in Ethers are on a timer, we can await for them to resolve using
+     * this function
+     */
+    await mockProvider.waitForEvents()
+
+    /** Notice how the balanceOf function returns the mocked value */
+    expect(mockedListener).toHaveBeenCalledWith(
+      fromAddress,
+      toAddress,
+      tokenId,
+      /** The last parameters is the actual event */
+      expect.anything()
+    )
+
+    mockProvider.removeAllListeners()
+
+    /** Clear all mocks so we leave a clean test */
+    mockProvider.clearMocks()
+  })
 })
